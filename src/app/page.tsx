7cf0fe6f1ae7b838900bae27722b5e0be6124284
Aditya Kamble <adityakamble6101@gmail.com>
2024-07-08 23:18:48 +0530

    add create and delete features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import prisma from "@/db";
-import assert from "assert";
 import { revalidatePath } from "next/cache";
 import Link from "next/link";
 
@@ -27,24 +26,50 @@ export default async function Home() {
             revalidatePath("/");
           }}
         >
-          <div>
-            <label htmlFor="title">Title</label>
-            <input type="text" name="title" />
+          <div className="w-full sm:max-w-sm">
+            <label
+              htmlFor="title"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Title
+            </label>
+            <input
+              type="text"
+              name="title"
+              className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
           </div>
-          <div>
+          <div className="mt-2 w-full sm:max-w-sm">
             <label htmlFor="content">Content</label>
-            <textarea name="content" />
+            <textarea
+              name="content"
+              className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
           </div>
-          <button>Create</button>
+          <button
+            type="submit"
+            className="flex justify-center rounded-md bg-indigo-600 px-3 py-1.5 my-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Create
+          </button>
         </form>
       </div>
 
       <ul className="py-4 list-disc list-inside">
         {posts.map((post) => (
-          <li key={post.id}>
+          <li key={post.id} className="flex space-x-2">
             <Link className="underline" href={`/post/${post.id}`}>
-              {post.title}
+              {post.title}{" "}
             </Link>
+            <form
+              action={async () => {
+                "use server";
+                await prisma.post.delete({ where: { id: post.id } });
+                revalidatePath("/");
+              }}
+            >
+              <button>X</button>
+            </form>
           </li>
         ))}
       </ul>
